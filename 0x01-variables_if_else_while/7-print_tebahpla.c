421aa95857bc566108069e95e9d7deb97e11bce9
Ayoyinka <kolawoleayoyinka.ka@gmail.com>
2022-09-09 18:22:26 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * display
+ *
+ * Return: 0 success
+ *
+ */
+
+int main(void)
+
+{
+
+	char a;
+
+	for (a = 122; a >= 97; a--)
+
+	{
+
+		putchar(a);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
