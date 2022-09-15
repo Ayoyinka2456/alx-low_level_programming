de25f538ff72ee23bf331f9fe288d28990e2df2c
Ayoyinka <kolawoleayoyinka.ka@gmail.com>
2022-09-15 10:15:06 -0700

    0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - To test if the
+ * positive_or_negative function works
+ *
+ *  Result: 0 if successful, 1 otherwise
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
