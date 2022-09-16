8382109c25d79656e3d5986c61d93b522bea283b
Ayoyinka <kolawoleayoyinka.ka@gmail.com>
2022-09-15 17:34:14 -0700

    1. isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - To look through for 0 -9
+ * @c: The argument to examine
+ * Return: 1 if 0-9 is found, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if ((c >= 48) && ((c <= 57))
+		{
+			return (1);
+		}
+	return (0);
+}
