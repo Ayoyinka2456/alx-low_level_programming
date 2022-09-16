da3b996037a48d568e2f4f789084e6a26e2438e9
Ayoyinka <kolawoleayoyinka.ka@gmail.com>
2022-09-15 17:03:10 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - chechs if a character is uppercase
+ * @c: character to be checked
+ * Return: 1 if uppercase, 0 if not uppercase
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (_putchar(1));
+	}
+	return (0)
+}
