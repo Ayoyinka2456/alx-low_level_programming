2f2505e338a42988f4e9d2df3511719331a7dd37
Ayoyinka <kolawoleayoyinka.ka@gmail.com>
2022-09-19 13:24:00 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - To change the initial value of the variable n to 98
+ * @n: The variable to be changed
+ * Result: 0 always
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
