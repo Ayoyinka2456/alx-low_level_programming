eba07ffd8a05c288cfbabf8f3f9f8cf08da664ff
Ayoyinka <kolawoleayoyinka.ka@gmail.com>
2022-09-29 15:14:55 -0700

    function that returns 1 if the input integer is a prime number, otherwise return 0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,6 +8,7 @@ int actual_prime(int n, int i);
  * Return: 1 if n is a prime number, 0 if not
  */
 
+
 int is_prime_number(int n)
 {
 if (n <= 1)
@@ -24,7 +25,7 @@ return (actual_prime(n, n - 1));
 
 int actual_prime(int n, int i)
 {
-if (n == 1)
+if (i == 1)
 	return (1);
 if (n % i == 0 && i > 0)
 	return (0);
