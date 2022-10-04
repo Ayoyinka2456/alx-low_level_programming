ae5a7f5aa225144b7eac26a18eb6cbbae0357efa
Ayoyinka <kolawoleayoyinka.ka@gmail.com>
2022-10-04 15:10:12 -0700

    Copying a string and allocating memory using malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strdup - Function that duplicates a string
+ * @str: The string to be duplicated
+ * Return: NULL if str is empty, new_array if successful
+ */
+
+char *_strdup(char *str)
+{
+	char *copy_string;
+	unsigned int i, j, size = i0;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		size++;
+
+	copy_string = malloc(size * sizeof(str));
+
+	for (j = 0; j < size; j++)
+		copy_string[j] = str[j];
+
+	return (copy_string);
+}
