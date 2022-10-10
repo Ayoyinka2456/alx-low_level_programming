06dbe0fbdc3d1d6c18618acff045f8834c478cc8
Ayoyinka <kolawoleayoyinka.ka@gmail.com>
2022-10-10 16:48:23 -0700

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog- A new object with attributes
+ * @name: Name of object
+ * @age: Age of object
+ * @owner: Owner of object
+ */
+
+
+struct dog = {
+	char *name;
+	int age;
+	char *owner;
+};
+
+#endif
